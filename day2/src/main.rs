71215af6375038701cc06605ca6bdbb600712219
JWebmeister <josh.webmeister@gmail.com>
2023-11-11 02:30:28 +1100

    Day 2 part 2 completed.

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -52,7 +52,7 @@ fn main() {
 #[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
 struct Round {
     theirs: Move,
-    ours: Move,
+    outcome: Outcome,
 }
 
 impl std::str::FromStr for Round {
@@ -67,32 +67,27 @@ impl std::str::FromStr for Round {
             }
         };
         c.next();
-        let ours: Move = match c.next() {
-            Some(ours) => ours.try_into()?,
+        let outcome: Outcome = match c.next() {
+            Some(outcome) => outcome.try_into()?,
             _ => {
                 return Err(());
             }
         };
-        Ok(Self { theirs, ours })
+        Ok(Self { theirs, outcome })
     }
 }
 
 impl Round {
-    fn outcome(self) -> Outcome {
-        if self.ours == self.theirs {
-            return Outcome::Draw;
-        };
-        if (self.ours as isize) == (self.theirs as isize + 1) {
-            return Outcome::Win;
-        };
-        if (self.ours == Move::Rock) && (self.theirs == Move::Scissors) {
-            return Outcome::Win;
-        };
-        Outcome::Loss
+    fn ours(self) -> Move {
+        match self.outcome {
+            Outcome::Loss => self.theirs.winvs(),
+            Outcome::Draw => self.theirs,
+            Outcome::Win => self.theirs.lossvs(),
+        }
     }
 
     fn score(self) -> isize {
-        (self.ours as isize) + (self.outcome() as isize)
+        (self.outcome as isize) + (self.ours() as isize)
     }
 }
 
@@ -103,14 +98,32 @@ enum Move {
     Scissors = 3,
 }
 
+impl Move {
+    fn winvs(self) -> Move {
+        match self {
+            Move::Rock => Move::Scissors,
+            Move::Paper => Move::Rock,
+            Move::Scissors => Move::Paper,
+        }
+    }
+
+    fn lossvs(self) -> Move {
+        match self {
+            Move::Rock => Move::Paper,
+            Move::Paper => Move::Scissors,
+            Move::Scissors => Move::Rock,
+        }
+    }
+}
+
 impl TryFrom<char> for Move {
     type Error = ();
 
     fn try_from(c: char) -> Result<Self, Self::Error> {
         match c {
-            'A' | 'X' => Ok(Move::Rock),
-            'B' | 'Y' => Ok(Move::Paper),
-            'C' | 'Z' => Ok(Move::Scissors),
+            'A' => Ok(Move::Rock),
+            'B' => Ok(Move::Paper),
+            'C' => Ok(Move::Scissors),
             _ => Err(()),
         }
     }
@@ -123,128 +136,59 @@ enum Outcome {
     Win = 6,
 }
 
+impl TryFrom<char> for Outcome {
+    type Error = ();
+
+    fn try_from(c: char) -> Result<Self, Self::Error> {
+        match c {
+            'X' => Ok(Outcome::Loss),
+            'Y' => Ok(Outcome::Draw),
+            'Z' => Ok(Outcome::Win),
+            _ => Err(()),
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    use std::str::FromStr;
-
     use super::*;
-
-    #[test]
-    fn rock_beats_scissors() {
-        assert_eq!(
-            Outcome::Win,
-            Round {
-                theirs: Move::Scissors,
-                ours: Move::Rock
-            }
-            .outcome()
-        );
-        assert_eq!(
-            Outcome::Loss,
-            Round {
-                theirs: Move::Rock,
-                ours: Move::Scissors
-            }
-            .outcome()
-        );
-    }
-
-    #[test]
-    fn paper_beats_rock() {
-        assert_eq!(
-            Outcome::Win,
-            Round {
-                theirs: Move::Rock,
-                ours: Move::Paper
-            }
-            .outcome()
-        );
-        assert_eq!(
-            Outcome::Loss,
-            Round {
-                theirs: Move::Paper,
-                ours: Move::Rock
-            }
-            .outcome()
-        );
-    }
-
-    #[test]
-    fn scissors_beats_paper() {
-        assert_eq!(
-            Outcome::Win,
-            Round {
-                theirs: Move::Paper,
-                ours: Move::Scissors
-            }
-            .outcome()
-        );
-        assert_eq!(
-            Outcome::Loss,
-            Round {
-                theirs: Move::Scissors,
-                ours: Move::Paper
-            }
-            .outcome()
-        );
-    }
-
-    #[test]
-    fn same_move_equals_draw() {
-        assert_eq!(
-            Outcome::Draw,
-            Round {
-                theirs: Move::Rock,
-                ours: Move::Rock
-            }
-            .outcome()
-        );
-        assert_eq!(
-            Outcome::Draw,
-            Round {
-                theirs: Move::Paper,
-                ours: Move::Paper
-            }
-            .outcome()
-        );
-        assert_eq!(
-            Outcome::Draw,
-            Round {
-                theirs: Move::Scissors,
-                ours: Move::Scissors
-            }
-            .outcome()
-        );
-    }
 
     #[test]
     fn move_from_char_works() {
         let a = Move::try_from('A').unwrap();
-        let x = Move::try_from('X').unwrap();
         assert_eq!(a, Move::Rock);
-        assert_eq!(x, Move::Rock);
 
         let b = Move::try_from('B').unwrap();
-        let y = Move::try_from('Y').unwrap();
         assert_eq!(b, Move::Paper);
-        assert_eq!(y, Move::Paper);
 
         let c = Move::try_from('C').unwrap();
-        let z = Move::try_from('Z').unwrap();
         assert_eq!(c, Move::Scissors);
-        assert_eq!(z, Move::Scissors);
 
         assert_eq!(Move::try_from(' '), Err(()));
+    }
+
+    #[test]
+    fn outcome_from_char_works() {
+        let x = Outcome::try_from('X').unwrap();
+        assert_eq!(x, Outcome::Loss);
+
+        let y = Outcome::try_from('Y').unwrap();
+        assert_eq!(y, Outcome::Draw);
+
+        let z = Outcome::try_from('Z').unwrap();
+        assert_eq!(z, Outcome::Win);
+
+        assert_eq!(Outcome::try_from(' '), Err(()));
     }
 
     #[test]
     fn round_from_string_works() {
         let s = "A Y";
         assert_eq!(
-            Round::from_str(s).unwrap(),
+            s.parse::<Round>().unwrap(),
             Round {
                 theirs: Move::Rock,
-                ours: Move::Paper
+                outcome: Outcome::Draw,
             }
         );
     }
@@ -252,30 +196,30 @@ mod tests {
     #[test]
     fn game_scoring_works() {
         let s = "A Y\nB X\nC Z";
-        let mut r = s.lines().map(|l| Round::from_str(l));
+        let mut r = s.lines().map(|l| l.parse::<Round>());
         let mut total_score = 0;
 
         let Some(Ok(x)) = r.next() else {
             return;
         };
-        assert_eq!(x.outcome(), Outcome::Win);
-        assert_eq!(x.score(), 8);
+        assert_eq!(x.ours(), Move::Rock);
+        assert_eq!(x.score(), 4);
         total_score += x.score();
 
         let Some(Ok(x)) = r.next() else {
             return;
         };
-        assert_eq!(x.outcome(), Outcome::Loss);
+        assert_eq!(x.ours(), Move::Rock);
         assert_eq!(x.score(), 1);
         total_score += x.score();
 
         let Some(Ok(x)) = r.next() else {
             return;
         };
-        assert_eq!(x.outcome(), Outcome::Draw);
-        assert_eq!(x.score(), 6);
+        assert_eq!(x.ours(), Move::Rock);
+        assert_eq!(x.score(), 7);
         total_score += x.score();
 
-        assert_eq!(total_score, 15);
+        assert_eq!(total_score, 12);
     }
 }
