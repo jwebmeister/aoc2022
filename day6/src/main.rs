9d25cd2080eb42f8925ed0c08f79bd10a722ce87
JWebmeister <josh.webmeister@gmail.com>
2023-11-16 23:40:00 +1100

    completed day6 part1

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,9 +1,11 @@
-use std::io::prelude::*;
 use thiserror::Error;
 
 fn main() {
-    let mut file_input = open_file().unwrap();
+    let file_input = open_file().unwrap();
     let mut reader = std::io::BufReader::new(file_input);
+    let marker_idx = find_marker_idx(&mut reader).unwrap().unwrap();
+
+    println!("Marker index = {}", marker_idx);
 }
 
 #[derive(Error, Debug)]
@@ -20,14 +22,13 @@ fn find_marker_idx<R: std::io::BufRead>(reader: &mut R) -> Result<Option<usize>,
     const MARKER_SIZE: usize = 4;
     let mut marker_idx: usize = MARKER_SIZE;
 
-    let mut n_buf = [0_u8; MARKER_SIZE * 4];
+    let mut n_buf = [0_u8; MARKER_SIZE];
     match reader.read_exact(&mut n_buf) {
         Ok(_) => {}
         Err(_) => return Err(MyError::NotEnoughData(MARKER_SIZE)),
     };
-    let mut c_buf = std::str::from_utf8(&n_buf)?;
 
-    match unique_chars(c_buf) {
+    match unique_chars(&n_buf) {
         Ok(b) => {
             if b {
                 return Ok(Some(marker_idx));
@@ -36,22 +37,38 @@ fn find_marker_idx<R: std::io::BufRead>(reader: &mut R) -> Result<Option<usize>,
         Err(e) => return Err(e),
     }
 
-    todo!()
-}
-
-fn unique_chars(s: &str) -> Result<bool, MyError> {
-    let n = s.len();
-    let mut bs: u32 = 0b_0;
-    for c in s.chars() {
-        bs |= char_to_u32_bitset(c)?;
+    let mut next_n = [0_u8; 1];
+    while reader.read_exact(&mut next_n).is_ok() {
+        marker_idx += 1;
+        match next_n[0].is_ascii_lowercase() {
+            true => n_buf[(marker_idx - 1) % MARKER_SIZE] = next_n[0],
+            false => return Err(MyError::NotLowercaseChar(next_n[0] as char)),
+        };
+        match unique_chars(&n_buf) {
+            Ok(b) => {
+                if b {
+                    return Ok(Some(marker_idx));
+                }
+            }
+            Err(e) => return Err(e),
+        }
     }
-    Ok(bs.count_ones() as usize == n)
+
+    Ok(None)
 }
 
-fn char_to_u32_bitset(c: char) -> Result<u32, MyError> {
-    let n = c as u8;
-    if !c.is_ascii_lowercase() {
-        return Err(MyError::NotLowercaseChar(c));
+fn unique_chars(n_buf: &[u8]) -> Result<bool, MyError> {
+    let count = n_buf.len();
+    let mut bs: u32 = 0b_0;
+    for n in n_buf {
+        bs |= u8_to_u32_bitset(*n)?;
+    }
+    Ok(bs.count_ones() as usize == count)
+}
+
+fn u8_to_u32_bitset(n: u8) -> Result<u32, MyError> {
+    if !n.is_ascii_lowercase() {
+        return Err(MyError::NotLowercaseChar(n as char));
     }
     Ok(1 << (n - b'a'))
 }
@@ -87,37 +104,61 @@ mod tests {
     use super::*;
 
     #[test]
-    fn char_to_u32_bitset_works() {
-        assert_eq!(char_to_u32_bitset('a').unwrap(), 0b_1);
-        assert_eq!(char_to_u32_bitset('b').unwrap(), 0b_10);
-        assert_eq!(char_to_u32_bitset('c').unwrap(), 0b_100);
+    fn u8_to_u32_bitset_works() {
+        assert_eq!(u8_to_u32_bitset('a' as u8).unwrap(), 0b_1);
+        assert_eq!(u8_to_u32_bitset('b' as u8).unwrap(), 0b_10);
+        assert_eq!(u8_to_u32_bitset('c' as u8).unwrap(), 0b_100);
         assert_eq!(
-            char_to_u32_bitset('z').unwrap(),
+            u8_to_u32_bitset('z' as u8).unwrap(),
             0b_10000000000000000000000000
         );
         assert!(matches!(
-            char_to_u32_bitset('A'),
+            u8_to_u32_bitset('A' as u8),
             Err(MyError::NotLowercaseChar('A'))
         ));
         assert!(matches!(
-            char_to_u32_bitset('0'),
+            u8_to_u32_bitset('0' as u8),
             Err(MyError::NotLowercaseChar('0'))
+        ));
+        assert!(matches!(
+            u8_to_u32_bitset(255 as u8),
+            Err(MyError::NotLowercaseChar(_))
         ));
     }
 
     #[test]
     fn unique_chars_works() {
-        assert!(unique_chars("abc").unwrap());
-        assert!(unique_chars("abcdef").unwrap());
-        assert!(unique_chars("zxy").unwrap());
-        assert!(!unique_chars("aaaaa").unwrap());
+        assert!(unique_chars(&[b'a', b'b', b'c']).unwrap());
         assert!(matches!(
-            unique_chars("AAA"),
+            unique_chars(&[b'A', b'A', b'A']),
             Err(MyError::NotLowercaseChar('A'))
         ));
         assert!(matches!(
-            unique_chars("0000"),
+            unique_chars(&[b'0', b'0', b'0']),
             Err(MyError::NotLowercaseChar('0'))
         ));
+    }
+
+    #[test]
+    fn find_marker_idx_works() {
+        let s = "mjqjpqmgbljsphdztnvjfqwrcgsmlb".as_bytes();
+        let mut reader = std::io::BufReader::new(s);
+        assert_eq!(Some(7), find_marker_idx(&mut reader).unwrap());
+
+        let s = "bvwbjplbgvbhsrlpgdmjqwftvncz".as_bytes();
+        let mut reader = std::io::BufReader::new(s);
+        assert_eq!(Some(5), find_marker_idx(&mut reader).unwrap());
+
+        let s = "nppdvjthqldpwncqszvftbrmjlhg".as_bytes();
+        let mut reader = std::io::BufReader::new(s);
+        assert_eq!(Some(6), find_marker_idx(&mut reader).unwrap());
+
+        let s = "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg".as_bytes();
+        let mut reader = std::io::BufReader::new(s);
+        assert_eq!(Some(10), find_marker_idx(&mut reader).unwrap());
+
+        let s = "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw".as_bytes();
+        let mut reader = std::io::BufReader::new(s);
+        assert_eq!(Some(11), find_marker_idx(&mut reader).unwrap());
     }
 }
