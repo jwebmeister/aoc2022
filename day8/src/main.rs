cd408b61a0dfeb4137a4792829c0c822d6328a82
JWebmeister <josh.webmeister@gmail.com>
2023-11-17 23:41:35 +1100

    added day8 hello world

---
+++ day8/src/main.rs
@@ -0,0 +1,29 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+fn open_file() -> std::io::Result<std::fs::File> {
+    let filepath_input = "./src/input.txt";
+    let alt_filepath_input = "./day8/src/input.txt";
+    let file_input = match std::fs::File::open(filepath_input) {
+        Ok(file) => {
+            println!("Opening {}", filepath_input);
+            file
+        }
+        Err(_) => match std::fs::File::open(alt_filepath_input) {
+            Ok(file) => {
+                println!("Opening {}", alt_filepath_input);
+                file
+            }
+            Err(e) => {
+                println!(
+                    "Unable to open input data file from {0} or {1}.",
+                    filepath_input, alt_filepath_input
+                );
+                println!("{}", e);
+                return Err(e);
+            }
+        },
+    };
+    Ok(file_input)
+}
