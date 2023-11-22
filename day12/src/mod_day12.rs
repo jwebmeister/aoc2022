248c6199138acd14318edf0e75610d189586227c
JWebmeister <josh.webmeister@gmail.com>
2023-11-22 18:04:31 +1100

    day12- added test, debug fmt for grid and cell

--- day12/src/mod_day12.rs
+++ day12/src/mod_day12.rs
@@ -5,7 +5,7 @@ pub enum MyError {
     #[error(transparent)]
     Io(#[from] std::io::Error),
     #[error("Row width doesn't match first row, error while parsing")]
-    ParseMismatchRowWidth
+    ParseMismatchRowWidth,
 }
 
 #[derive(Clone, Copy)]
@@ -15,11 +15,38 @@ enum Cell {
     Square(u8),
 }
 
+impl std::fmt::Debug for Cell {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Cell::Start => write!(f, "S"),
+            Cell::End => write!(f, "E"),
+            Cell::Square(n) => write!(f, "{}", (n + b'a') as char),
+        }
+    }
+}
+
 struct Grid {
     width: usize,
     height: usize,
     data: Vec<Cell>,
 }
+
+impl std::fmt::Debug for Grid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f)?;
+        for row in 0..self.height {
+            let idx_start = row * self.width;
+            let idx_end = (row + 1) * self.width;
+            let line = self.data[idx_start..idx_end]
+                .iter()
+                .map(|n| format!("{:?}", n))
+                .collect::<String>();
+            writeln!(f, "{}", line)?;
+        }
+        Ok(())
+    }
+}
+
 
 impl std::ops::Index<(usize, usize)> for Grid {
     type Output = Cell;
@@ -40,11 +67,14 @@ fn parse_into_grid<R: std::io::BufRead>(mut reader: R) -> Result<Grid, MyError> 
     let mut first_line = String::new();
     reader.read_line(&mut first_line)?;
 
-    let first_v = first_line.chars().filter_map(parse_cell).collect::<Vec<_>>();
+    let first_v = first_line
+        .chars()
+        .filter_map(parse_cell)
+        .collect::<Vec<_>>();
 
     let width = first_v.len();
-    let mut height: usize = 0;
     let mut data: Vec<Cell> = Vec::with_capacity(width.pow(2));
+    let mut height: usize = 1;
 
     data.extend(first_v);
 
@@ -52,11 +82,17 @@ fn parse_into_grid<R: std::io::BufRead>(mut reader: R) -> Result<Grid, MyError> 
         let l = line?;
         let mut v_line = Vec::with_capacity(width);
         v_line.extend(l.chars().filter_map(parse_cell));
-        if v_line.len() != width {return Err(MyError::ParseMismatchRowWidth)};
+        if v_line.len() != width {
+            return Err(MyError::ParseMismatchRowWidth);
+        };
         data.extend(v_line);
         height += 1;
+    }
+    let grid = Grid {
+        width,
+        height,
+        data,
     };
-    let grid = Grid{ width, height, data };
     Ok(grid)
 }
 
@@ -66,5 +102,27 @@ fn parse_cell(c: char) -> Option<Cell> {
         'E' => Some(Cell::End),
         'a'..='z' => Some(Cell::Square(c as u8 - b'a')),
         _ => None,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_into_grid_works() {
+        #[rustfmt::skip]
+        let s = 
+"Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi";
+
+        let reader = std::io::BufReader::new(s.as_bytes());
+
+        let g = parse_into_grid(reader).unwrap();
+
+        assert_eq!(s, format!("{:?}", g).trim());
     }
 }
