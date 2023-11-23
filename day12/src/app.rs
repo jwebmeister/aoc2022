e4ac54d8f24a7f1a543bf4fbd13f43d8da7f0f8d
JWebmeister <josh.webmeister@gmail.com>
2023-11-23 20:44:18 +1100

    day12 wasm

--- day12/src/app.rs
+++ day12/src/app.rs
@@ -293,3 +293,8 @@ fn execute<F: Future<Output = ()> + Send + 'static>(f: F) {
     // this is stupid... use any executor of your choice instead
     std::thread::spawn(move || futures::executor::block_on(f));
 }
+
+#[cfg(target_arch = "wasm32")]
+fn execute<F: Future<Output = ()> + 'static>(f: F) {
+    wasm_bindgen_futures::spawn_local(f);
+}
--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,3 +1,4 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 use day12::MyApp;
 
 #[cfg(not(target_arch = "wasm32"))]
@@ -9,4 +10,23 @@ fn main() -> eframe::Result<()> {
         native_options,
         Box::new(|cc| Box::new(MyApp::new(cc))),
     )
+}
+
+#[cfg(target_arch = "wasm32")]
+fn main() {
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::WebRunner::new()
+            .start(
+                "the_canvas_id", // hardcode it
+                web_options,
+                Box::new(|cc| Box::new(MyApp::new(cc))),
+            )
+            .await
+            .expect("failed to start eframe");
+    });
 }
